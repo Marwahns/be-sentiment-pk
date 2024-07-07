ed80f8fe8946715ae5538b0ef104cd0ddaf9a33b
Marwah Nur Shafira <marwahnurshafira1705@gmail.com>
2024-07-07 22:33:34 +0700

    chore: Deploying model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 
 from fastapi import FastAPI
+import uvicorn
 from pydantic import BaseModel
 from typing import Union
 
@@ -61,3 +62,6 @@ async def post(text: str):
         "text": text,
         "predictions": result_predictions
     }
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
